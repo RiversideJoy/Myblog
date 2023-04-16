6862ff1b3391fe0355590f1ef55e3a7ca6b7baa8
wangbeijing <2231101379@qq.com>
2023-04-16 23:03:21 +0800

    init committ

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+const nav = require('./nav.js')
+module.exports = {
+  base: './vuepress',
+  // base: '/',
+  title: "麦仁博客",
+  description: "记录前端知识、读书学习",
+  markdown: {
+    lineNumbers: true,
+  },
+  plugins: {
+    
+  },
+  sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
+  // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | <自定义>    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
+  theme: 'vdoing',
+  themeConfig: {
+    nav,
+    sidebar: 'structuring',
+  },
+  // 使目录栏支持h2~h6
+  markdown: {
+    extractHeaders: [ 'h2', 'h3', 'h4', 'h5', 'h6' ]
+  }
+};
---
+++ docs/README.md
@@ -0,0 +1,21 @@
+---
+home: true
+heroText: 麦仁博客
+tagline: 一个分享各类资源的地方
+actionText: 查看资源
+actionLink: /软件资源/软件资源
+features: # 可选的
+  - title: 前端
+    details: JavaScript、ES6、Vue框架等前端技术
+    link: /web/ # 可选
+    imgUrl: /img/web.png # 可选
+  - title: 页面
+    details: html(5)/css(3)，前端页面相关技术
+    link: /ui/
+    imgUrl: /img/ui.png
+  - title: 技术
+    details: 技术文档、教程、技巧、总结等文章
+    link: /technology/
+    imgUrl: /img/other.png
+footer: 麦仁 | 前端开发
+---
