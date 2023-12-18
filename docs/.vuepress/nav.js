1698beed547e86aa23586bdb2a2b733151172f2e
wangbeijing <2231101379@qq.com>
2023-12-18 15:34:27 +0800

    测试

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -52,12 +52,7 @@ module.exports = [
   { text: '关于', link: '/about/' },
   {
     text: '收藏',
-    link: '/pages/beb6c0bd8a66cea6/',
-    // items: [
-    //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
-    //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
-    //   { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
-    // ],
+    link: '/pages/beb6c0bd8a66cea6/'
   },
   {
     text: '索引',
